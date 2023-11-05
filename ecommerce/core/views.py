fca79a2a32c2cab8fd705375ac68110c6a171e16
Bruno Vieira Barbosa <bruno.vieira.barbosa.255@gmail.com>
2023-11-05 01:27:57 -0300

    Altera a view para retornar o template

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Pagina Inicial')
+    return render(request, 'core/index.html')
